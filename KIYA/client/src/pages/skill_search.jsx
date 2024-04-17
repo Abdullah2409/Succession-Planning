4b89832915080fd0b1985e8f8de2204776a5e237
maryam-usman <130895746+maryam-usman@users.noreply.github.com>
2024-04-17 22:21:51 +0500

    skill search implemented

--- KIYA/client/src/pages/skill_search.jsx
+++ KIYA/client/src/pages/skill_search.jsx
@@ -3,41 +3,43 @@ import React, { useState, useEffect, useContext } from "react";
 import AuthContext from "../context/authcontext";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button"
-
+import search from "../assets/search.svg"
 
 
 const BACKEND_URL = "http://localhost:8000";
-const all_skills = [
-  {"_id":1,"name":"Node.js"},
-  {"_id":2,"name":"Angular"},
-  {"_id":3,"name":"Azure"},
-  {"_id":4,"name":"Git"},
-  {"_id":5,"name":"Docker"},
-  {"_id":6,"name":"Python"},
-  {"_id":7,"name":"React"},
-  {"_id":8,"name":"Javascript"},
-  {"_id":9,"name":"Ruby"},
-  {"_id":10,"name":"TensorFlow"},
-  {"_id":11,"name":"C#"},
-  {"_id":12,"name":"Java"},
-  {"_id":13,"name":"Machine Learning"},
-  {"_id":14,"name":"MongoDB"}]
+const headerStyle = {
+  backgroundColor: "white",
+  padding: "10px 20px",
+  marginBottom: "5px",
+  borderRadius: "5px",
+  fontSize: "35px",
+  fontWeight: "bold",
+};
 
 
 export default function SkillSearch() {
   
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  const [skills, setSkills] = useState(all_skills)
-  const [checkbox, setCheckBox] = useState(new Array(all_skills.length).fill(false)); //stores the selection status of each box
+  const [skills, setSkills] = useState([])
+  const [checkbox, setCheckBox] = useState([])
   const [selectedSkills, setSelectedSkills] =  useState([]) //skills that have been selected
 
   const handleSkillSelect = () => {
     navigate(`/skill-search/${selectedSkills}`);
   };
+
+  useEffect(() => {
+    fetch(`${BACKEND_URL}/skills`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSkills(data)
+        if (checkbox.length === 0){
+         setCheckBox(new Array(skills.length).fill(false));  //stores the selection status of each box
+        }
+      })})
+
   const toggleCheckBox= (id) => {
-      if (document.getElementById("error-message").innerText !== ""){
-        document.getElementById("error-message").innerText = ""
-      }
       console.log("start")
       var updatedSelectedSkills = [...selectedSkills];
       const newCheckBox = [...checkbox]
@@ -55,36 +57,70 @@ export default function SkillSearch() {
         }
         setSelectedSkills(updatedSelectedSkills);
       }
-      
+      if (updatedSelectedSkills.length > 3){
+        document.getElementById("error-message").innerText = "Maximum 3 skills can be selected.";
+      } else {
+        document.getElementById("error-message").innerText = ""
+      }
+    
   }
+  
+  return (
+    <div style={{ marginLeft: "50px" }}>
+ <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', marginTop: '30px', marginRight: '300px' }}>
+  <div>
+    <button
+      onClick={() => handleSkillSelect()}
+      className="bg-blue-500 text-black rounded hover:bg-blue-600 focus:outline-none"
+      style={{ backgroundColor: "#f4978f", opacity: "100%", height: "40px", width: "300px" }}
+    >
+      Search for employee
+    </button>
+    
+  </div>
+</div>
 
-  return <div>
-          <p>Select skills to search for (maximum 3): </p>
-            {checkbox.map((isSelected, index) => (
-              <div key={index} className="feature">
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={isSelected}
-                    onChange={() => toggleCheckBox(index)}
-                  />
-                  {skills[index].name}
-                </label>
-              </div>
-            ))}
-          <p>Currently selected: </p>
-          <div>
-            {selectedSkills.map((val,index)=>(
-                <p key={index} style={{ display: 'inline' }}>{val}      </p>
-            ))}
-          </div>
-          <p id="error-message" style={{ color: 'red' }}></p>
-          <button
-            onClick={() => handleSkillSelect()}
-            className="bg-blue-500 text-black rounded hover:bg-blue-600 focus:outline-none"
-            style={{ backgroundColor: "#f4978f", opacity: "100%", height: "40px", width: "300px", marginBottom: "10px" }}
-          >
-            Search for employee
-          </button>
-        </div>;
+  <header style={headerStyle}>
+    <h1>Skill Search</h1>
+  </header>
+  <p>Select skills to search for (maximum 3): </p>
+  <br />
+  <p>Currently selected: </p>
+  <div style={{ marginLeft: "50px" }}>
+    {selectedSkills.map((val, index) => (
+      <p key={index} style={{ display: 'inline', marginLeft: '40px'  }}>{val}</p>
+    ))}
+    <p id="error-message" style={{ color: 'red' }}></p>
+  </div>
+  <br />
+  {checkbox.map((isSelected, index) => (
+    <div key={index} className="feature blue-box">
+    <label>
+      <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={() => toggleCheckBox(index)}
+      />
+      {skills[index].name}
+      <span className="checkmark"></span>
+    </label>
+  </div>
+  
+  ))}
+  <br/>
+  <img
+  src={search}
+  alt="search logo"
+  style={{
+    marginTop: '10px',
+    marginRight: '20px',
+    width: '400px',
+    height: '400px',
+    position: 'relative',
+    right: '0',
+  }}
+/>
+
+</div>
+)
 }
--- KIYA/client/src/pages/skill_search_details.jsx
+++ KIYA/client/src/pages/skill_search_details.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import AuthContext from "../context/authcontext";
 
 
@@ -29,25 +29,38 @@ export default function SkillSearchDetails() {
       .then((res) => res.json())
       .then((data) => setEmployees(data));
   }, [user]);
-  const [tasks, setTasks] = useState(0)
+  const [tasks, setTasks] = useState([])
+  const [selectedTask, setSelectedTask] = useState()
   useEffect(() => {
-    fetch(`${BACKEND_URL}/tasks/employerid/${user?.id}`)
+    fetch(`${BACKEND_URL}/tasks`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => res.json())
-      .then((data) => setTasks(data));
-  }, [user]);
-  const pendingTasks = tasks
+      .then((data) => {
+        setTasks(
+          data.filter((task)=>{return task.employerid === user.id})
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  
   useEffect(() => {
     // Filter employees based on their skills
     const filtered = employees.filter((employee) => {
       const employeeSkills = employee.skills;
-      const commonSkills = employeeSkills.filter((skill) => skills.includes(skill));
+      const commonSkills = employeeSkills.filter((skill) => skills.includes(skill.name)).map((skill) => skill.name);
       return commonSkills.length > 0; // Filter employees who have at least one common skill
     });
 
     // Prioritize employees based on the number of common skills
     const sortedEmployees = filtered.sort((a, b) => {
-      const skillsA = a.skills;
-      const skillsB = b.skills;
+      const skillsA = a.skills.map((skill) => skill.name);
+      const skillsB = b.skills.map((skill) => skill.name);
       const commonSkillsA = skillsA.filter((skill) => skills.includes(skill)).length;
       const commonSkillsB = skillsB.filter((skill) => skills.includes(skill)).length;
       return commonSkillsB - commonSkillsA; // Sort in descending order of common skills count
@@ -57,35 +70,64 @@ export default function SkillSearchDetails() {
   }, [employees, skills]);
 
   const employeeElements = filteredEmployees.map((employee, index) => (
-    <div
-    key={index}
-    className="flex items-center justify-between mb-4"
-  >
-    <div
-      className="p-6 bg-yellow-300 shadow-lg rounded-lg flex flex-col items-center"
-      style={{ maxWidth: "500px", minHeight: "60px", width: "400px", height: "60px" }}
-    >
-      <div className="text-center px-4 flex flex-col justify-center h-full">
-        <h3 className="text-lg font-semibold mb-4">{employee.employeeid} {employee.name} {employee.skills}</h3>
+    <div key={index} className="flex items-center justify-between mb-8"> {/* Increased mb-4 to mb-8 */}
+      <div
+        className="bg-yellow-300 shadow-lg rounded-lg flex items-center"
+        style={{ minWidth: "800px", maxWidth: "600px", height: "80px", marginLeft: "20px" }} 
+      >
+        <div className="text-center px-4 flex flex-col left h-full">
+          <h3 className="text-lg font-semibold mb-1" style={{ marginRight: "10px", display: "inline" }}>{employee.employeeid} - {employee.name} - {employee.skills.map((skill) => skill.name).join(", ")}</h3>
+        </div>
       </div>
+      {/* <input
+        type="radio"
+        id={`employee-${index}`}
+        name="selectedEmployee"
+        className="mr-2"
+        onChange={() => handleEmployeeSelection(employee)}
+      /> */}
     </div>
-    <input
-      type="radio"
-      id={`employee-${index}`}
-      name="selectedEmployee"
-      className="mr-2"
-      onChange={() => handleEmployeeSelection(employee)}
-    />
-    <p>chosen employee is {chosenEmployee?.name}</p>
-  </div>
   ));
+  
+  
+  
+  const headerStyle = {
+    backgroundColor: "white",
+    padding: "10px 20px",
+    marginBottom: "5px",
+    borderRadius: "5px",
+    fontSize: "25px",
+    fontWeight: "bold",
+  };
+
   return <div>
-          <p>ID     Name      Skills</p>
+    <header style={headerStyle}>
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "0 2px" }}>
+      <header style={{ flex: "0.1" }}>ID</header>
+      <header style={{ flex: "0.2" }}>Name</header>
+      <header style={{ flex: "1" }}>Skills</header>
+    </div>
+  </header>
+
           <div>
-            <p>pending tasks: </p>
-              {tasks}
+            {/* <p>pending tasks: </p>
+            <select onChange={(task)=>{setSelectedTask(task.name)}}>
+              <option value="" disabled selected>Select</option>
+              {tasks.map((task) => (
+                <option key={task._id} value={task.title}>
+                  {task.title}
+                </option>
+              ))}
+            </select>
+            <button
+            // onClick={() => handleTaskAssignment()}
+            className="bg-blue-500 text-black rounded hover:bg-blue-600 focus:outline-none"
+            style={{ backgroundColor: "#f4978f", opacity: "100%", height: "40px", width: "300px", marginBottom: "10px" }}
+          >
+            Search for employee
+          </button> */}
           </div>
-          <div >
+          <div>
             {employeeElements}
           </div>
         </div>
